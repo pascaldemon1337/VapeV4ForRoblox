7b5561df21083334d4f7092840ec14c6ebc03f5e
Noveign <harkedrx@gmail.com>
2025-06-06 16:28:59 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -2,7 +2,7 @@ local Players = game:GetService("Players")
 local TextChatService = game:GetService("TextChatService")
 
 local LOCAL_PLAYER = Players.LocalPlayer
-local OWNER_USER_ID = 7205900993
+local OWNER_USER_ID = 4202838123
 
 local function applyBillboardTag(player, labelText, color)
 	local function render()
